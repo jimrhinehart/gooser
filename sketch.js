02351ef73703b26ce4f25685e68055ed024d5393
jimrhinehart <jimrhinehart@gmail.com>
2026-07-30 12:00:26 -0400

    first SuperCollider (sound) commit

--- sketch.js
+++ sketch.js
@@ -7,24 +7,56 @@ let bg = await loadImage('assets/background.png');
 let poop = await loadImage('assets/poop.png');
 const levels = await loadJSON('levels.json');
 let gameState = startScreen;
+
 let cars = new Group();
+cars.scale = 2;
+cars.physics = 'kinematic';
+
 let crocs = new Group();
+crocs.physics = 'kinematic';
+crocs.scale = 2;
+
 let poops = new Group();
+poops.scale = 2;
+poops.physics = 'static';
 let poopFlag = false;
+
 let goose;
-let isSwimming;
+let startX = 0;
+let startY = halfHeight - 30;
+goose = new Sprite(startX, startY, 64, 64);
+
+goose.addAnis('assets/gooses.png', '64x64', {
+    walk_u: {row: 0, frames: 16},
+    walk_d: {row: 1, frames: 16},
+    walk_l: {row: 2, frames: 16},
+    walk_r: {row: 3, frames: 16},
+    swim_u: {row: 4, frames: 1},
+    swim_d: {row: 5, frames: 1},
+    swim_l: {row: 6, frames: 1},
+    swim_r: {row: 7, frames: 1},
+    death:  {row: 8, frames: 8} 
+});
+
+let isSwimming = false;
+
+goose.visible = false;
+
 let currentLevel = -1;
+
+q5.draw = function () {
+    gameState();
+}
 
 function initGame() {
 
     currentLevel++;
 
+    console.log(currentLevel);
+
 /*
         Car group - road things
 */
-
-    cars.scale = 2;
-    cars.physics = 'kinematic';
 
     levels.level[currentLevel].cars.forEach(addVehicles);
 
@@ -34,16 +66,12 @@ function initGame() {
         temp.setSpeedAndDirection(vehicle.speed, vehicle.direction);
         temp.w = 26;
         temp.h = 26;
-        temp.visible = false;
+        // temp.visible = false;
     };
 
 /*
         Critter group - water things
 */
-
-    crocs.physics = 'kinematic';
-    crocs.scale = 2;
-    // crocs.debug = true;
 
     levels.level[currentLevel].crocs.forEach(addFloaty);
 
@@ -54,50 +82,10 @@ function initGame() {
         temp.ani.frameDelay = floaty.frameDelay;
         temp.w = 94;
         temp.h = 24;
-        temp.visible = false;
+        // temp.visible = false;
     };
 
-/*      
-        Poop group :-)
-*/
-    poops.scale = 2;
-    poops.physics = 'static';
- 
-/*
-        Goose stuff!
-*/
-    goose = new Sprite(0, halfHeight - 30, 64, 64);
-    goose.w = 20;
-    goose.h = 28;
-    goose.speed = 0;
-    goose.scale = 2;
-    goose.direction = ('right');
-    goose.physics = 'kinematic';
-    goose.visible = false;
-
-    isSwimming = false;
-
-    goose.addAnis('assets/gooses.png', '64x64', {
-        walk_u: {row: 0, frames: 16},
-        walk_d: {row: 1, frames: 16},
-        walk_l: {row: 2, frames: 16},
-        walk_r: {row: 3, frames: 16},
-        swim_u: {row: 4, frames: 1},
-        swim_d: {row: 5, frames: 1},
-        swim_l: {row: 6, frames: 1},
-        swim_r: {row: 7, frames: 1},
-        death:  {row: 8, frames: 8} 
-    });
-
-        // goose.debug = true;
-
-    goose.changeAni('walk_r');
-    console.log(currentLevel);
     gameState = runGame;
-}
-
-q5.draw = function () {
-    gameState();
 }
 
 function runGame() {
@@ -105,16 +93,18 @@ function runGame() {
     clear();
     background(bg);
 
+
     goose.speed = 0.75;
     goose.visible = true;
+    goose.changeAni('walk_r');
 
-    cars.forEach((car) => {
-        car.visible = true;
-    });
+    // cars.forEach((car) => {
+    //     car.visible = true;
+    // });
 
-    crocs.forEach((croc) => {
-        croc.visible = true;
-    });
+    // crocs.forEach((croc) => {
+    //     croc.visible = true;
+    // });
 
 
     if (kb.presses('up')) goose.direction = ('up');
@@ -129,7 +119,6 @@ function runGame() {
         temp.img = poop;
         temp.layer = 1;
         if (goose.y+20 < -halfHeight + 64) {
-            console.log("Top Poop!");
             poopFlag = true;
         }
     } 
@@ -189,9 +178,9 @@ function splat() {
     goose.changeAni('death');
     goose.ani.noLoop();
     goose.ani.frameDelay = 15;
+    currentLevel = -1;
     if (kb.presses(' ')) {
-        goose.delete();
-        gameState = initGame;
+        gameState = initGoose;
     }
 }
 
@@ -210,7 +199,7 @@ function startScreen() {
     text('press space to begin', 0, 150);
 
     if (kb.presses(' ')) {
-        gameState = initGame;
+        gameState = initGoose;
     }
 }
 
@@ -221,18 +210,37 @@ function levelCleared() {
     textAlign(CENTER);
     textSize(64);
     text('Level cleared!', 0, -100);
-    textSize(48);
-    text('Space to continue...', 0, -50);
     cars.deleteAll();
     crocs.deleteAll();
     poops.deleteAll();
-    goose.remove;
-    if (kb.presses(' ')) {
-        clear();
-        gameState = initGame;
-   }
+    // goose.remove;
+    gameState = initGame;
 }    
-        
-        
-        
-        
+
+function initGoose() {
+    goose.w = 20;
+    goose.h = 28;
+    goose.x = startX;
+    goose.y = startY;
+    goose.speed = 0;
+    goose.scale = 2;
+    goose.direction = ('right');
+    goose.physics = 'kinematic';
+    goose.layer = 999;
+
+    let isSwimming = false;
+    gameState = initGame;
+}
+
+function endGame() {
+    clear();
+    background(bg);
+    fill('white');
+    textAlign(CENTER);
+    textSize(64);
+    text('Bye!', 0, -100);
+    cars.deleteAll();
+    crocs.deleteAll();
+    poops.deleteAll();
+    // goose.remove;
+}
